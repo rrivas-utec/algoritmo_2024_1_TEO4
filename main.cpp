ade0eead99030e3412a482886a0529e24253a910
rrivas <rrivas@upc.edu.pe>
2024-05-16 17:54:03 -0500

    Update repo

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <deque>
 using namespace std;
 
-using Iterador = vector<int>::iterator;
 
 bool EsImpar(int value) {
     return value % 2 != 0;
@@ -19,6 +18,7 @@ bool MayorA6(T value) {
 }
 
 // Puntero a funcion
+using Iterador = vector<int>::iterator;
 using Callable = bool (*) (int);
 
 
@@ -31,8 +31,8 @@ auto crear_vector(Iterador inicio, Iterador fin, Callable condicion) {
     return result;
 }
 
-template<template<typename...>class TContainer = vector, typename Iterator, typename Unary>
-auto crear_container(Iterator inicio, Iterator fin, Unary condicion) {
+template<template<typename...>class TContainer = vector, typename Iterator, typename UnaryPredicate>
+auto crear_container(Iterator inicio, Iterator fin, UnaryPredicate condicion) {
     TContainer<typename Iterator::value_type> result;
     for (auto it = inicio; it != fin; ++it)
         if(condicion(*it)) {
@@ -42,9 +42,17 @@ auto crear_container(Iterator inicio, Iterator fin, Unary condicion) {
 }
 
 void ejemplo_crear_vector() {
+    vector<int> v1 = {10, 2, 4, 7, 3, 10, 6, 9, 8};
+    auto v2 = crear_vector(begin(v1), end(v1), EsPar);
+    auto v3 = crear_container(begin(v1), end(v1), MayorA6<double>);
+    for (const auto& item: v2) {
+        cout << item << " ";
+    }
+}
+
+void ejemplo_crear_container() {
     vector<double> v1 = {10, 2, 4, 7, 3, 10, 6, 9, 8};
-    // auto v2 = crear_vector(begin(v1), end(v1), MayorA6);
-    auto v2 = crear_container(begin(v1), end(v1), MayorA6<double>);
+    auto v2 = crear_container(begin(v1), end(v1), [](auto v) { return v > 6; });
     for (const auto& item: v2) {
         cout << item << " ";
     }
@@ -65,8 +73,39 @@ void crear_puntero_a_print() {
 
 }
 
+void crear_lambda() {
+    auto lambda_sumar = [](auto a, auto b) { return a + b; };
+    cout << lambda_sumar(10, 30) << endl;
+
+    cout << [](auto a, auto b) { return a + b; }(10, 20);
+
+    double pi = 3.1415;
+    auto calcular_area_elipse = [pi](auto a, auto b) mutable {
+//        pi = 3.14;
+        return a*b*pi;
+    };
+    cout << calcular_area_elipse(10, 20);
+    pi = 3.141596;
+    cout << calcular_area_elipse(10, 20);
+}
+
+class calcular_area_elipse {
+    const double pi;
+public:
+    explicit calcular_area_elipse(double pi): pi(pi) {}
+    double operator()(double a, double b) const { return a*b*pi; }
+};
+
+void ejemplo_funcion_objeto() {
+    double pi = 3.1415;
+    auto cae = calcular_area_elipse(pi);
+    cout << cae(10, 20);
+};
+
 int main() {
-    // ejemplo_crear_vector();
-    crear_puntero_a_print();
+//    ejemplo_crear_vector();
+//    ejemplo_crear_container();
+    ejemplo_funcion_objeto();
+//    crear_puntero_a_print();
     return 0;
 }
